92b6e2d1556a9582219c56fdeaa91829f316c291
Gale <gale00242@gmail.com>
2019-01-09 15:40:55 +0800

    添加项目文件。

---
+++ GPay.Admin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GPay.Admin.Startup))]
+namespace GPay.Admin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
